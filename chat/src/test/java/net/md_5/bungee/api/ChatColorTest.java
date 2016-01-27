46e7f1c0e5a622f866f6d8b99d64b7796c9ad042
Tux <write@imaginarycode.com>
2016-01-26 23:34:06 -0500

    Added unit tests to ChatColor static methods

---
+++ chat/src/test/java/net/md_5/bungee/api/ChatColorTest.java
@@ -0,0 +1,30 @@
+package net.md_5.bungee.api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ChatColorTest {
+
+    @Test
+    public void testStripColor() throws Exception {
+        assertEquals("Test without colors", ChatColor.stripColor("Test without colors"));
+        assertEquals("Test with red", ChatColor.stripColor(ChatColor.RED + "Test with red"));
+        assertEquals("Test with credential", ChatColor.stripColor("Test with " + ChatColor.RED + "credential"));
+        assertEquals("Test with special formatting", ChatColor.stripColor("Test with " + ChatColor.ITALIC + "special" +
+                ChatColor.RESET + " formatting"));
+    }
+
+    @Test
+    public void testTranslateAlternateColorCodes() throws Exception {
+        // "normal" case
+        assertEquals(ChatColor.RED + "test", ChatColor.translateAlternateColorCodes('&', "&ctest"));
+        // odder but still valid cases
+        assertEquals(ChatColor.AQUA + "test", ChatColor.translateAlternateColorCodes('^', "^btest"));
+        assertEquals(ChatColor.AQUA + "test", ChatColor.translateAlternateColorCodes('^', "^Btest"));
+
+        // invalid case
+        assertEquals("^ztest", ChatColor.translateAlternateColorCodes('^', "^ztest"));
+    }
+
+}

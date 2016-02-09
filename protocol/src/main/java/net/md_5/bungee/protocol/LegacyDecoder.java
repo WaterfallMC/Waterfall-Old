a6790f3460e73e4682137fd258f172fa0b8eb753
Techcable <Techcable@outlook.com>
2016-02-08 19:48:07 -0700

    Check if legacy ping packets are valid, instead of asuming they are
    Prevents misinterpretation of legacy ping packets as handshakes.
    Should fix #27
    
    If at the begining of a connection it sees '0xFE' it assumes that it is a legacy ping packet.
    Handshake packets may also start with '0xFE' though, which confuses bungee.
    
    Bungee assumes its a ping, but doesn't do any validation at all.
    Then it chokes on the partially-processed handshake packet.
    This can cause all sorts of different exceptions, depending on the mood bungee is in, but they all trace back to the LegacyDecoder.
    
    The solution is to check if the packet really is a legacy ping, and treat it normally if so.
    Vanilla also does this, and encounters none of these issues.
    
    I've tested that legacy pings still work with this commit.

--- protocol/src/main/java/net/md_5/bungee/protocol/LegacyDecoder.java
+++ protocol/src/main/java/net/md_5/bungee/protocol/LegacyDecoder.java
@@ -5,11 +5,15 @@ import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.ByteToMessageDecoder;
 import java.util.List;
+
+import com.google.common.base.Charsets;
+
 import net.md_5.bungee.protocol.packet.LegacyHandshake;
 import net.md_5.bungee.protocol.packet.LegacyPing;
 
 public class LegacyDecoder extends ByteToMessageDecoder
 {
+    private boolean triedForMoreData;
 
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception
@@ -22,18 +26,61 @@ public class LegacyDecoder extends ByteToMessageDecoder
         in.markReaderIndex();
         short packetID = in.readUnsignedByte();
 
-        if ( packetID == 0xFE )
-        {
-            out.add( new PacketWrapper( new LegacyPing( in.isReadable() && in.readUnsignedByte() == 0x01 ), Unpooled.EMPTY_BUFFER ) );
-            return;
-        } else if ( packetID == 0x02 && in.isReadable() )
-        {
-            in.skipBytes( in.readableBytes() );
-            out.add( new PacketWrapper( new LegacyHandshake(), Unpooled.EMPTY_BUFFER ) );
-            return;
+        boolean handled = false;
+        try {
+            if (packetID == 0xFE) {
+                final LegacyPingType pingType;
+                switch (in.readableBytes()) {
+                    case 0:
+                        pingType = LegacyPingType.ANCIENT;
+                        break;
+                    case 1:
+                        if (in.readUnsignedByte() != 0x01) return; // Not a ping
+                        pingType = LegacyPingType.v14_v15;
+                        break;
+                    default:
+                        // Validate the 1.6 ping
+                        if (in.readUnsignedByte() != 0x01) return; // Not a ping
+                        if (in.readUnsignedByte() != 0xFA) return; // Invalid 1.6 ping (possibly a handshake)
+                        if (!readLegacyString(in).equals("MC|PingHost")) return;
+                        int lengthOfRestOfData = in.readShort();
+                        int protocolVersion = in.readUnsignedByte();
+                        if (protocolVersion < 73) return; // Invalid protocol version
+                        String hostname = readLegacyString(in);
+                        if (hostname.length() * 2 + 7 != lengthOfRestOfData) return; // Invalid length
+                        int port = in.readInt();
+                        if (port < 0 || port > 0xFFFF) return; // Invalid port
+                        if (in.readableBytes() > 0) return; // We still have more (and shouldn't)
+                        pingType = LegacyPingType.v16;
+                        break;
+                }
+                out.add(new PacketWrapper(new LegacyPing(pingType != LegacyPingType.ANCIENT), Unpooled.EMPTY_BUFFER));
+                handled = true;
+            } else if (packetID == 0x02 && in.isReadable()) {
+                in.skipBytes(in.readableBytes());
+                out.add(new PacketWrapper(new LegacyHandshake(), Unpooled.EMPTY_BUFFER));
+                handled = true;;
+            }
+        } catch (RuntimeException ignored) {
+            handled = false;
+        } finally {
+            if (!handled) {
+                in.resetReaderIndex();
+                ctx.pipeline().remove(this);
+            }
         }
+    }
 
-        in.resetReaderIndex();
-        ctx.pipeline().remove( this );
+    public static String readLegacyString(ByteBuf buf) {
+        int stringLength = buf.readShort();
+        byte[] stringData = new byte[stringLength * 2];
+        buf.readBytes(stringData);
+        return new String(stringData, Charsets.UTF_16BE);
+    }
+
+    enum LegacyPingType {
+        ANCIENT,
+        v14_v15,
+        v16;
     }
 }

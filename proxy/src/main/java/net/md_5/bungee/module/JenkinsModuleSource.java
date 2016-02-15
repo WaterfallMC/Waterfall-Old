f6a1bd11d1c91592b8d8fcd1137246a533583a02
Tux <write@imaginarycode.com>
2016-02-15 01:39:20 -0500

    Don't make a full temporary copy in memory when downloading modules

--- proxy/src/main/java/net/md_5/bungee/module/JenkinsModuleSource.java
+++ proxy/src/main/java/net/md_5/bungee/module/JenkinsModuleSource.java
@@ -1,10 +1,11 @@
 package net.md_5.bungee.module;
 
-import com.google.common.io.ByteStreams;
-import com.google.common.io.Files;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+
 import lombok.Data;
 import net.md_5.bungee.Util;
 
@@ -25,7 +26,7 @@ public class JenkinsModuleSource implements ModuleSource
             con.setReadTimeout( 15000 );
             con.setRequestProperty("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36");
 
-            Files.write( ByteStreams.toByteArray( con.getInputStream() ), module.getFile() );
+            Files.copy(con.getInputStream(), module.getFile().toPath(), StandardCopyOption.REPLACE_EXISTING);
             System.out.println( "Download complete" );
         } catch ( IOException ex )
         {

e80073c1b67a7d71591913a4304f0b71f50dd2b8
Tux <write@imaginarycode.com>
2016-02-15 01:45:50 -0500

    Raise max log size to 8MB

--- proxy/src/main/java/net/md_5/bungee/log/BungeeLogger.java
+++ proxy/src/main/java/net/md_5/bungee/log/BungeeLogger.java
@@ -27,7 +27,7 @@ public class BungeeLogger extends Logger
 
         try
         {
-            FileHandler fileHandler = new FileHandler( System.getProperty("bungee.log-file", "proxy.log"), 1 << 22, 8, false );
+            FileHandler fileHandler = new FileHandler( System.getProperty("bungee.log-file", "proxy.log"), 1 << 23, 8, false );
             fileHandler.setFormatter( formatter );
             addHandler( fileHandler );
 

96ccea2f2fa77635c4ef6582f1b8b4c5a48b20e7
Tux <write@imaginarycode.com>
2016-03-26 22:04:20 -0400

    Revert "ReduceJavaZlib GC similar to what is implemented in JavaCipher"
    This reverts commit d35522483252f27266cec8c24b05df898363c4f6.
    
    Seems to have issues

--- native/src/main/java/net/md_5/bungee/jni/zlib/JavaZlib.java
+++ native/src/main/java/net/md_5/bungee/jni/zlib/JavaZlib.java
@@ -8,18 +8,7 @@ import java.util.zip.Inflater;
 public class JavaZlib implements BungeeZlib
 {
 
-    private static final ThreadLocal<byte[]> heapInLocal = new ThreadLocal<byte[]>() {
-        @Override
-        protected byte[] initialValue() {
-            return new byte[0];
-        }
-    };
-    private static final ThreadLocal<byte[]> heapOutLocal = new ThreadLocal<byte[]>() {
-        @Override
-        protected byte[] initialValue() {
-            return new byte[8192];
-        }
-    };
+    private final byte[] buffer = new byte[ 8192 ];
     //
     private boolean compress;
     private Deflater deflater;
@@ -56,8 +45,8 @@ public class JavaZlib implements BungeeZlib
     @Override
     public void process(ByteBuf in, ByteBuf out) throws DataFormatException
     {
-        byte[] inData = bufToByte( in );
-        byte[] buffer = heapOutLocal.get();
+        byte[] inData = new byte[ in.readableBytes() ];
+        in.readBytes( inData );
 
         if ( compress )
         {
@@ -66,8 +55,8 @@ public class JavaZlib implements BungeeZlib
 
             while ( !deflater.finished() )
             {
-                int count = deflater.deflate(buffer);
-                out.writeBytes(buffer, 0, count );
+                int count = deflater.deflate( buffer );
+                out.writeBytes( buffer, 0, count );
             }
 
             deflater.reset();
@@ -77,24 +66,11 @@ public class JavaZlib implements BungeeZlib
 
             while ( !inflater.finished() )
             {
-                int count = inflater.inflate(buffer);
-                out.writeBytes(buffer, 0, count );
+                int count = inflater.inflate( buffer );
+                out.writeBytes( buffer, 0, count );
             }
 
             inflater.reset();
         }
-    }
-
-    private byte[] bufToByte(ByteBuf in)
-    {
-        byte[] heapIn = heapInLocal.get();
-        int readableBytes = in.readableBytes();
-        if ( heapIn.length < readableBytes )
-        {
-            heapIn = new byte[ readableBytes ];
-            heapInLocal.set( heapIn );
-        }
-        in.readBytes( heapIn, 0, readableBytes );
-        return heapIn;
     }
 }

4eb5175503ab9f4e313c37ef68ec9e8dcc890a7c
Tux <write@imaginarycode.com>
2016-05-08 02:40:35 -0400

    Simplified the command completer.
    The console thread is going to block on another thread anyway, so just tab complete in the main thread. Also, invalid commands use a shortcut path.

--- proxy/src/main/java/net/md_5/bungee/ConsoleCommandCompleter.java
+++ proxy/src/main/java/net/md_5/bungee/ConsoleCommandCompleter.java
@@ -1,24 +1,14 @@
 package net.md_5.bungee;
 
 import com.google.common.collect.Iterables;
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import jline.console.completer.Completer;
 import net.md_5.bungee.api.ProxyServer;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.logging.Level;
 
 public class ConsoleCommandCompleter implements Completer
 {
-
-    private final ExecutorService executor = Executors.newSingleThreadExecutor(
-            new ThreadFactoryBuilder().setNameFormat( "Console Command Completer Thread - %1$d" ).build() );
 
     private final ProxyServer proxy;
 
@@ -30,43 +20,21 @@ public class ConsoleCommandCompleter implements Completer
     @Override
     public int complete( String buffer, int cursor, List<CharSequence> candidates )
     {
-        Future<Iterable<String>> future = executor.submit( new Callable<Iterable<String>>()
+        List<String> offers = new ArrayList<>();
+        if ( !proxy.getPluginManager().dispatchCommand( proxy.getConsole(), buffer, offers ) )
         {
-            @Override
-            public Iterable<String> call() throws Exception
-            {
-                List<String> results = new ArrayList<>();
-                proxy.getPluginManager().dispatchCommand( proxy.getConsole(), buffer, results );
-                return results;
-            }
-        } );
-
-        try
-        {
-            Iterable<String> offers = future.get();
-            if ( offers == null )
-            {
-                return cursor;
-            }
-
-            Iterables.addAll( candidates, offers );
-
-            final int lastSpace = buffer.lastIndexOf( ' ' );
-            if ( lastSpace == -1 )
-            {
-                return cursor - buffer.length();
-            } else
-            {
-                return cursor - ( buffer.length() - lastSpace - 1 );
-            }
-        } catch ( ExecutionException ex )
-        {
-            proxy.getLogger().log( Level.WARNING, "Unhandled exception when tab completing", ex );
-        } catch ( InterruptedException ex )
-        {
-            Thread.currentThread().interrupt();
+            return cursor;
         }
 
-        return cursor;
+        Iterables.addAll( candidates, offers );
+
+        final int lastSpace = buffer.lastIndexOf( ' ' );
+        if ( lastSpace == -1 )
+        {
+            return cursor - buffer.length();
+        } else
+        {
+            return cursor - ( buffer.length() - lastSpace - 1 );
+        }
     }
 }

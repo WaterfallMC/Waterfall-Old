1dc4f402443d992b6e16ccb57f64b0fe8ae4b0c7
Tux <write@imaginarycode.com>
2016-05-19 17:36:31 -0600

    Better unit tests for Chat API

--- chat/src/test/java/net/md_5/bungee/api/chat/TranslatableComponentTest.java
+++ chat/src/test/java/net/md_5/bungee/api/chat/TranslatableComponentTest.java
@@ -3,6 +3,8 @@ package net.md_5.bungee.api.chat;
 import net.md_5.bungee.api.chat.TranslatableComponent;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class TranslatableComponentTest
@@ -14,5 +16,26 @@ public class TranslatableComponentTest
         TranslatableComponent testComponent = new TranslatableComponent( "Test string with %s placeholders: %s", "2", "aoeu" );
         assertEquals( "Test string with 2 placeholders: aoeu", testComponent.toPlainText() );
         assertEquals( "§fTest string with §f2§f placeholders: §faoeu", testComponent.toLegacyText() );
+    }
+
+    @Test
+    public void testDuplicateNullWithDoesntThrowException() {
+        TranslatableComponent component = new TranslatableComponent("Test") {
+            @Override
+            public List<BaseComponent> getExtra() {
+                return null;
+            }
+        };
+
+        TranslatableComponent copy = new TranslatableComponent(component);
+        // The fact that we don't throw an exception means it's working as intended.
+    }
+
+    @Test
+    public void testEscapedPercentInPlainText()
+    {
+        TranslatableComponent testComponent = new TranslatableComponent( "Test string with %% sign" );
+        assertEquals( "Test string with % sign", testComponent.toPlainText() );
+        assertEquals( "§fTest string with §f%§f sign", testComponent.toLegacyText() );
     }
 }
